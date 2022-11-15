1da8f9887ca07ba0c3242320d9fd9da4bdcd7fa2
parabolang <105626277+parabolang@users.noreply.github.com>
2022-11-16 03:03:16 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import json
+import numpy as np
 
 yourpath = 'labels'
-
+count=np.zeros((20,), dtype=int)
+print(count[0])
 for root, dirs, files in os.walk(yourpath, topdown=False):
     for name in files:
         print(os.path.join(root, name))
@@ -11,6 +13,16 @@ for root, dirs, files in os.walk(yourpath, topdown=False):
         result = str(data)
         print("The frame of yes:",result.count(" 1"))
         print("Total frame:", result.count(":"))
+        i=os.path.join(root, name)
+        print(i[len(i)-6])
+        print(type(i[2]))
+        for iter_var in range( len( count ) ):
+            if i[len(i)-6] == str(iter_var) and  i[len(i)-7] != str(1):
+                count[iter_var]=count[iter_var]+result.count(" 1")
+            elif i[len(i)-6] == str(iter_var) and i[len(i)-7] == str(1):
+                count[iter_var]=count[iter_var]+result.count(" 1")
         f.close
     #for name in dirs:
        #print(os.path.join(root, name))
+print (count)
+
